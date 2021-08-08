7bb79112cefe1620fa3af709e0e626eafb834a78
proencaf <83221947+proencaf@users.noreply.github.com>
2021-08-08 18:01:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// Create a website that checks surf data for a beach in the UK
+// make sure you figure out if API is working + free ( don't waste time)
+// simple functionality (fetch 1 or 2 parameters **only**)
+// param 1 (wave size) param 2 (wind or temp)
+
+// css plan
+// go for simple but "nice" looking website
+// add hover effects and keyframes on button and or text div.
+// use coolors and shadowlord for colors
+//experiment with position relative, absolute,fixed
+
+// keep it simpple
+
+// ----- Wave size api---only 50 calls a day for free usage and only 1 specific location can be fetched, newquay has been selected)
+const lat = 50.4163;
+const lng = -5.1001;
+const params = "waveHeight";
+const pElement = document.querySelector("#wave-size");
+const buttonElement = document.querySelector("#button");
+const textElement = document.querySelector(".text-field");
+
+async function getWaves() {
+  const response = await fetch(
+    `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`,
+    {
+      headers: {
+        Authorization:
+          "16c56b64-f855-11eb-9f40-0242ac130002-16c56bdc-f855-11eb-9f40-0242ac130002",
+      },
+    }
+  );
+
+  const data = await response.json();
+  //console.log(data);
+  //const waveData = data.hours[0].waveHeight.noaa;
+  //console.log(waveData);
+  const allWaveData = data.hours.map((x) => x.waveHeight.noaa);
+  //console.log(allWaveData);
+  //let waveSize = waveData.waveHeight;
+  //console.log(waveSize);
+  //let waveNoaa = waveSize.noaa;
+  //console.log(waveNoaa);
+  //const waveAverage = allWaveData.reduce((a, b) => a + b, 0) / 241;
+  //console.log(waveAverage);
+  //const waveSizeDay = Math.round(waveAverage);
+  //console.log(waveSizeDay);
+  const highestWave = Math.max(...allWaveData);
+  const smallestWave = Math.min(...allWaveData);
+  pElement.innerText = `Today's biggest wave in Newquay is ${highestWave} m and the smallest is ${smallestWave} m`;
+}
+
+buttonElement.addEventListener("click", getWaves);
